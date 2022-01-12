1c22fcb385c1e812ed6787b985147a19e00a0143
DphnZwp <zwuupdaphne@gmail.com>
2022-01-12 23:24:12 +0100

    Basisopzet website #13
    De basis structuur van de website en een klein beetje css.

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// var card = document.querySelector(".card");
+
+// document.querySelector(document).addEventListener("mousemove",function(e) {  
+//   var ax = -(document.querySelector(window).innerWidth()/2- e.pageX)/20;
+//   var ay = (document.querySelector(window).innerHeight()/2- e.pageY)/10;
+//   card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
+// });
